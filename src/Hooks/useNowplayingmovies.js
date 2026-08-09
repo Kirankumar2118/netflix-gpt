f5ad4c9ebf74acf42876aa4400f8427111447486
kirankumar belakeri <belakerikirankumar@gmail.com>
2026-08-09 22:23:29 +0530

    updted hooks

--- src/Hooks/useNowplayingmovies.js
+++ src/Hooks/useNowplayingmovies.js
@@ -1,26 +1,27 @@
-import { useCallback, useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 
-import { options, url } from "../Utils/Apioptions";
+import { fetchDatafromApi } from "../Utils/Api";
 import { addnowplayingmovies } from "../Redux/Movieslice";
 
 const useNowplayingmovies = () => {
   const dispatch = useDispatch();
 
-  const getnowplaying = useCallback(async () => {
-    try {
-      const data = await fetch(url, options);
-      const json = await data.json();
-
-      dispatch(addnowplayingmovies(json));
-    } catch (error) {
-      console.error("Now playing movies error:", error);
-    }
-  }, [dispatch]);
-
   useEffect(() => {
-    getnowplaying();
-  }, [getnowplaying]);
+    const getNowPlaying = async () => {
+      try {
+        const data = await fetchDatafromApi("/movie/now_playing", { page: 1 });
+
+        if (data?.results) {
+          dispatch(addnowplayingmovies(data));
+        }
+      } catch (error) {
+        console.error("Now playing movies error:", error);
+      }
+    };
+
+    getNowPlaying();
+  }, [dispatch]);
 };
 
 export default useNowplayingmovies;
