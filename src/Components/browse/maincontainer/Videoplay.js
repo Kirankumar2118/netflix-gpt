ff1c1000ee869b8409b2cf873012e737463a1524
kirankumar belakeri <belakerikirankumar@gmail.com>
2026-08-09 21:38:29 +0530

    updated i frame

--- src/Components/browse/maincontainer/Videoplay.js
+++ src/Components/browse/maincontainer/Videoplay.js
@@ -7,8 +7,8 @@ const Videoplay = ({ movieid }) => {
 
   useGettrailer(movieid);
 
-  if (!trailer) {
-    return <div className="absolute inset-0 bg-black animate-pulse" />;
+  if (!trailer?.key) {
+    return null;
   }
 
   return (
@@ -17,27 +17,28 @@ const Videoplay = ({ movieid }) => {
       <iframe
         className="
           absolute
-          top-1/2
           left-1/2
-          w-screen
+          top-1/2
           h-[56.25vw]
-          min-h-screen
-          min-w-[177.77vh]
+          w-[177.78vw]
+          min-h-[100vh]
+          min-w-[177.78vh]
           -translate-x-1/2
           -translate-y-1/2
           border-0
           pointer-events-none
           select-none
         "
-        src={`https://www.youtube.com/embed/${trailer.key}?autoplay=1&mute=1&controls=0&loop=1&playlist=${trailer.key}&playsinline=1&rel=0&modestbranding=1&iv_load_policy=3&disablekb=1`}
+        src={`https://www.youtube.com/embed/${trailer.key}?autoplay=1&mute=1&playsinline=1`}
         title="Movie Trailer"
-        allow="autoplay; encrypted-media"
+        allow="autoplay; encrypted-media; picture-in-picture"
         allowFullScreen
       />
 
       {/* Left Gradient */}
       <div
         className="
+          pointer-events-none
           absolute
           inset-0
           bg-gradient-to-r
@@ -50,30 +51,32 @@ const Videoplay = ({ movieid }) => {
       {/* Bottom Gradient */}
       <div
         className="
+          pointer-events-none
           absolute
           inset-x-0
           bottom-0
           h-56
-          sm:h-64
-          md:h-72
           bg-gradient-to-t
           from-black
           via-black/70
           to-transparent
+          sm:h-64
+          md:h-72
         "
       />
 
       {/* Top Gradient */}
       <div
         className="
+          pointer-events-none
           absolute
           inset-x-0
           top-0
           h-24
-          md:h-36
           bg-gradient-to-b
           from-black/80
           to-transparent
+          md:h-36
         "
       />
     </section>
